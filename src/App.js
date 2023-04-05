3b6c23747033e36a0ab81801cac6610b522eecf5
Aunalipatel <69858342+Aunalipatel@users.noreply.github.com>
2023-04-05 10:16:09 +0530

    c7

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
-import { Button, Container, Box, Paper } from "@mui/material";
+import {
+  Button,
+  Container,
+  Box,
+  Paper,
+  ThemeProvider,
+  CssBaseline,
+  createTheme,
+} from "@mui/material";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function App() {
   const [info, SetInfo] = useState([]);
   const [lastrefreshed, SetLastrefreshed] = useState("");
@@ -22,7 +37,7 @@ function App() {
 
   const fetchInfo = async () => {
     try {
-      const res = await axios.get("http://13.233.40.50:5000/");
+      const res = await axios.get("https://6d7c5d3b8ab4.ngrok.app");
       console.log("response", res.data);
       SetInfo(res.data);
       toast.success("Refreshed!", {
@@ -33,7 +48,7 @@ function App() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "colored",
+        theme: "light",
       });
       const date = new Date();
       const showTime =
@@ -46,58 +61,61 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Box m={5}>
-        <Button variant="contained" onClick={fetchInfo}>
-          Refresh
-        </Button>
-        <p>Last Refreshed at {lastrefreshed}</p>
-      </Box>
-      {info.map((s) => (
-        <Paper variant="outlined" elevation={2}>
-          <Box
-            sx={{
-              p: 0,
-              bgcolor: "background.default",
-              display: "grid",
-              gridTemplateColumns: { md: "1fr" },
-              gap: 0,
-            }}
-          >
-            <h4> Strategy: {s.Strategy}</h4>
-            <p>
-              {s.Instrument} , <b>{s.Signal} </b>
-            </p>
-            {s.Signal === "No Signal" ? (
-              <p></p>
-            ) : (
-              <div>
-                <p>
-                  {" "}
-                  <b>{s.Direction}</b> Direction, {s.Lots} Lots
-                </p>
-                <p>
-                  EntryDateTime : {s.EntryDateTime} , EntryPrice: {s.EntryPrice}{" "}
-                  , StopLoss: {s.StopLoss} , Target Price:
-                  {s.TargetPrice}
-                </p>
-                {/* {s.Signal === "Exit" ? (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <Box m={5}>
+          <Button variant="contained" onClick={fetchInfo}>
+            Refresh
+          </Button>
+          <p>Last Refreshed at {lastrefreshed}</p>
+        </Box>
+        {info.map((s) => (
+          <Paper variant="outlined" elevation={2}>
+            <Box
+              sx={{
+                p: 0,
+                bgcolor: "background.default",
+                display: "grid",
+                gridTemplateColumns: { md: "1fr" },
+                gap: 0,
+              }}
+            >
+              <h4> Strategy: {s.Strategy}</h4>
+              <p>
+                {s.Instrument} , <b>{s.Signal} </b> , {s.Ticker}
+              </p>
+              {s.Signal === "No Signal" ? (
+                <p></p>
+              ) : (
+                <div>
+                  <p>
+                    {" "}
+                    <b>{s.Direction}</b> Direction, {s.Lots} Lots
+                  </p>
+                  <p>
+                    EntryDateTime : {s.EntryDateTime} , EntryPrice:{" "}
+                    {s.EntryPrice} , StopLoss: {s.StopLoss} , Target Price:
+                    {s.TargetPrice}
+                  </p>
+                  {/* {s.Signal === "Exit" ? (
                   <div>
-                    <p>
-                      ExitDateTime : {s.ExitDateTime} , ExitPrice: {s.ExitPrice}{" "}
-                      , ExitCondition : {s.ExitCondition}
-                    </p>
+                  <p>
+                  ExitDateTime : {s.ExitDateTime} , ExitPrice: {s.ExitPrice}{" "}
+                  , ExitCondition : {s.ExitCondition}
+                  </p>
                   </div>
-                ) : (
+                  ) : (
                   <p></p>
                 )} */}
-              </div>
-            )}
-          </Box>
-        </Paper>
-      ))}
-      <ToastContainer />
-    </div>
+                </div>
+              )}
+            </Box>
+          </Paper>
+        ))}
+        <ToastContainer />
+      </div>
+    </ThemeProvider>
   );
 }
 
